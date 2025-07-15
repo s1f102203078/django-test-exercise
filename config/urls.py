f0eaf950b11065ddcb491562a0aafd5504ff9702
1F10230292 <s1f102302922@iniad.org>
2025-07-15 15:08:31 +0900

    delete機能追加

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', todo_views.index, name='index'),
     path('<int:task_id>/', todo_views.detail, name='detail'),
     path('delete/<int:task_id>/',todo_views.delete,name='delete'),
+    path('<int:task_id>/update', todo_views.update, name='update')
 ]
